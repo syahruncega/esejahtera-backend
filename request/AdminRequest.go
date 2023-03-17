f4edb97f5494afab0523b8a95271dc38c6ece21d
Fachry Haswanto <fachry111@gmail.com>
2023-03-18 01:32:53 +0800

    minor fix

--- request/AdminRequest.go
+++ request/AdminRequest.go
@@ -3,11 +3,11 @@ package request
 type CreateAdminRequest struct {
 	UserId        int    `json:"userId" binding:"required"`
 	NamaLengkap   string `json:"namaLengkap"`
-	UrlFotoProfil string `json:"urlFotProfil"`
+	UrlFotoProfil string `json:"urlFotoProfil"`
 }
 
 type UpdateAdminRequest struct {
 	UserId        int    `json:"userId"`
 	NamaLengkap   string `json:"namaLengkap"`
-	UrlFotoProfil string `json:"urlFotProfil"`
+	UrlFotoProfil string `json:"urlFotoProfil"`
 }
